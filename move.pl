3e4017fb3ccdac7b9d1f3c3eb1c63735a995fe6f
Matt Reinhart <matt@antitheoretical.org>
2011-01-06 19:21:31 -0500

    This might work.

--- move.pl
+++ move.pl
@@ -5,6 +5,7 @@ use warnings;
 our $READ_TIMEOUT = 10; # seconds
 
 use Device::SerialPort;
+use IO::File;
 
 unless (scalar(@ARGV) == 4){
   print STDERR qq(Usage:  $0 <DIRECTION> <STEPS> <DELAY_SECONDS> <COUNT>\n);
@@ -19,21 +20,54 @@ unless ($ARGV[1] =~ /^\d+$/ && $ARGV[2] =~ /^\d+$/ && $ARGV[3] =~ /^\d+$/){
   print STDERR qq(STEPS, DELAY_SECONDS, and COUNT must all be integers!\n);
 }
 
+my $port = Device::SerialPort->new("/dev/ttyUSB0")
+          || die "Couldn't initialize motor driver!\n";
+
+$port->baudrate(115200);
+$port->databits(8);
+$port->parity("none");
+$port->stopbits(1);
+$port->read_char_time(0);       # don't wait for each character
+$port->read_const_time(1000);   # 1 second per unfulfilled "read" call
+
 main();
 
 sub main{
-  my $port = Device::SerialPort->new("/dev/ttyUSB0")
-            || die "Couldn't initialize motor driver!\n";
-  
-  $port->baudrate(115200);
-  $port->databits(8);
-  $port->parity("none");
-  $port->stopbits(1);
-  $port->read_char_time(0);       # don't wait for each character
-  $port->read_const_time(1000);   # 1 second per unfulfilled "read" call
-  
-  my $time = 1;
+  my ($count,$saw) = read_from_serial();
+  my ($direction,$move_count,$move_delay,$move_repeat) = @ARGV;
+  chomp($move_repeat);
+  for (1..$move_repeat){
+    my $write_count = spin_motor($direction,$move_count);
+    sleep($move_delay);
+    ($count,$saw) = read_from_serial();
+  }
+}
+
+sub spin_motor {
+  my ($direction,$move_count) = @_;
+  my $write_count = $port->write(qq($direction$move_count;));
+  my $multiplier = $direction =~ /L/i ? -1 : 1;
+  adjust_location($move_count*$multiplier);
+  return $write_count;
+}
+
+sub adjust_location {
+  my $adjustment = shift || 0;
+  my $location = get_stored_location();
+  $location += $adjustment;
+  my $fh = IO::File->new("> /var/log/stepper/location.txt");
+  print $fh $location;
+}
+
+sub get_stored_location {
+  my $fh = IO::File->new("< /var/log/stepper/location.txt");
+  my $location = defined($fh) ? <$fh> : 0;
+  return $location;
+}
+
+sub read_from_serial {
   my ($count,$saw);
+  my $time = 1;
   do{
     ($count,$saw)=$port->read(1);
     if ($count > 0){
@@ -46,12 +80,5 @@ sub main{
       $time++;
     }
   } until ($time == $READ_TIMEOUT);
-  
-  my ($direction,$move_count,$move_delay,$move_repeat) = @ARGV;
-  chomp($move_repeat);
-  
-  for (1..$move_repeat){
-    my $write_count = $port->write(qq($direction$move_count;));
-    sleep($move_delay);
-  }
+  return ($count,$saw);
 }
